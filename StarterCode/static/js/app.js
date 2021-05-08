961b389e32e0ec6680dcb45b4248a9b2e8bdb2d4
pamhinostroza <toxicmrmade@yahoo.com>
2021-05-08 18:35:08 -0400

    redo

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,92 +1,114 @@
-
-
-function buildMetaData (sampleNumber) {
-
-d3.json("samples.json").then((data) => {
-    metadata = data.metadata;
+function buildMetaData(sampleNumber) {
+  d3.json("samples.json").then((data) => {
+    var metadata = data.metadata;
     var sample = metadata.filter(d => d.id == sampleNumber);
-    sample = sample [0]
+    var sample = sample[0]
     var metadata_object = d3.select("#sample-metadata");
     metadata_object.html("")
-    Object.entries(sample).forEach([key, value]) => {
-        metadata_object.append("h6").text(`${key.toUpperCase()}: ${value}`);
+    Object.entries(sample).forEach(([key, value]) => {
+      metadata_object.append("h6").text(`${key.toUpperCase()}: ${value}`);
     })
-})
-}
+  })
+};
+
 function buildCharts(sampleNumber) {
-    d3.json("samples.json").then((data) => {
-        samples = data.samples;
-        var sample = samples.filter(d => d.id == sampleNumber);
-        sample = sample[0]
-        var otu_ids = sample.otu_ids
-        var otu_labels = sample.otu_labels;
-        var sample_values = sample.sample_values;
+  d3.json("samples.json").then((data) => {
+    var samples = data.samples;
+    var sample = samples.filter(d => d.id == sampleNumber);
+    sample = sample[0]
+    var otu_ids = sample.otu_ids;
+    var otu_labels = sample.otu_labels;
+    var sample_values = sample.sample_values;
+    var metadata = data.metadata;
+    var sample = metadata.filter(d => d.id == sampleNumber);
+    var sample = sample[0]
+    var wfreq = sample.wfreq;
 
-        barTrace = {
-            y: otu_ids.slice(0,10).map(otu => `OTU ${otu}`).reverse(),
-            x: sample_values.slice(0,10).reverse(),
-            type: "bar",
-            text: otu_labels.slice(0,10).reverse(),
-            orientation: "h"
-        }
-        var layout = {
-            title: "Top 10 OTU",
-            yaxis:{
-            tickmode:"linear",
-            },
-            margin: {
-            l: 100,
-            r: 100,
-            t: 30,
-            b: 20
-            }
-            };
-
-Plotly.newplot("bar", [barTrace], layout);
-
-var trace1 = {
-    x: samples.otu_ids,
-    y: samples.sample_values,
-    mode: "markers",
-    marker: {
-    size: samples.sample_values,
-    color: samples.otu_ids
-    },
-    text: samples.otu_labels
-    };
-
+    barTrace = {
+      y: otu_ids.slice(0, 10).map(otu => `OTU ${otu}`).reverse(),
+      x: sample_values.slice(0, 10).reverse(),
+      marker: {
+        color: ["red", "green", "purple", "yellow", "blue", "orange", "grey", "pink", "teal", "peru"],
+        opacity: 0.6
+      },
+      type: "bar",
+      text: otu_labels.slice(0, 10).reverse(),
+      orientation: "h"
+    }
     var layout = {
-        xaxis:{title: "OTU ID"},
-        height: 600,
-        width: 1300
-        };
+      title: "Bar Chart",
+      xaxis: { title: "Sample Values" },
+      yaxis: { title: "OTU IDs" }
+    };
+    Plotly.newPlot("bar", [barTrace], layout);
 
-        var data1 = [trace1];
+    var bubbleTrace = {
+      x: otu_ids,
+      y: sample_values,
+      mode: 'markers',
+      colorscale: 'Earth',
+      marker: {
+        color: sample_values,
+        opacity: 0.6,
+        size: sample_values
+      },
+      text: otu_labels
+    };
+    var data = [bubbleTrace];
+    var layout = {
+      title: 'Bubble Chart',
+      showlegend: false,
+      xaxis: { title: "OTU IDs" },
+      yaxis: { title: "Sample Values" }
+    };
+    Plotly.newPlot('bubble', data, layout);
 
-        Plotly.newPlot("bubble", data1, layout); 
-})};
-
-var metadata = data.metadata;
-var result = metadata.filter(meta => meta.id.toString() === id)[0];
-var demographicInfo = d3.select("#sample-metadata");
-demographicInfo.html("");
-function optionChanged(id) {
-getInfo(id);
-}
+    // the pie chart doesnt take in the opacity, how do i make it happen?
+    var data = [{
+      values: otu_ids.slice(0, 10),
+      labels: sample_values,
+      type: 'pie',
+      marker: {
+        color: sample_values,
+        opacity: 0.6
+      }
+    }];
+    var layout = {
+      title: "Pie Chart"
+    };
+    Plotly.newPlot('pie', data, layout);
+    
+    // how do i change the format for the gauge?
+    // how do i add a subtitle to it?
+    var data = [
+      {
+        gauge: {
+          axis: { range: [null, 9]}},
+        value: wfreq,
+        title: { text: "Belly Button Washing Frequency (Weekly Scrubs)" },
+        type: "indicator",
+        mode: "gauge+number"
+      }
+    ];
+    var layout = { margin: { t: 0, b: 0 } };
+    Plotly.newPlot('gauge', data, layout);
+  })
+};
 
 function init() {
-var dropdown = d3.select("#selDataset");
-d3.json("./samples.json").then((data)=> {
-console.log(data)
-
-data.names.forEach(function(name) {
-dropdown.append("option").text(name).property("value");
-});
-getPlot(data.names[0]);
-getInfo(data.names[0]);
-});
+  var dropdown = d3.select("#selDataset");
+  d3.json("samples.json").then((d) => {
+    var samplenames = d.names;
+    samplenames.forEach((sample) => {
+      dropdown.append("option").text(sample).property("value", sample);
+    })
+    var firstsample = samplenames[0];
+    buildMetaData(firstsample);
+    buildCharts(firstsample);
+  });
 }
-data.names.forEach(function(name) {
-    dropdown.append("option").text(name).property("value");
-    });
 init();
+function optionChanged(sampleNumber) {
+  buildMetaData(sampleNumber);
+  buildCharts(sampleNumber);
+};
